ef7ac68b8c5e73356933397b1adc74a6592cb6ae
Kyrylo Zakurdaiev <25233423+kerilz@users.noreply.github.com>
2024-03-19 20:53:44 -0600

    historic process instances overview

--- src/main/java/org/l21s/insurance/camunda/Application.java
+++ src/main/java/org/l21s/insurance/camunda/Application.java
@@ -2,12 +2,33 @@ package org.l21s.insurance.camunda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String... args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public FilterRegistrationBean processCorsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOriginPattern("http://localhost:8081");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(0);
+    return bean;
   }
 
 }
